1fd66967178887a2b60a5f3b0fe9fc5da42ba9a3
andresfc <andrU_2194@hotmail.com>
2014-10-09 19:43:13 -0500

    wewwe

---
+++ lab1.cpp
@@ -0,0 +1,55 @@
+/**
+@file lab1.cpp
+@brief Se hace un breve descipción del codigo
+@author ANDRES CORREA, NEIL GUTIERREZ
+@date 05/09/2014
+
+
+Esta es una funcion para saber el algoritmo de huffman
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+
+int i, n, n1, aleatorio, P=1, U=10;
+char txt[50];
+int frecu();
+/**
+@brief funcion a probar
+@param argc 
+@param argv 
+
+*/
+
+int main(int argc,char *argv[])
+{ 
+
+/*Petición y contador de caracteres de frase a codificar*/
+printf("Introduzca una frase:\n"); 
+
+for(n=0;(txt[n]=getchar()) != '\n';++n); 
+txt[n]='\0'; 
+
+printf("La palabra %s tiene %d letras.\n",txt,n); 
+
+
+/*Generación de número al azar por letra introducida*/
+
+int i,j,max=txt[n],letras,con=0;
+	for(i=0;i<max;i++);
+    {
+    	for(j=0;j<max;j++)
+    	{
+    		if(letras[i]==letras[j])
+            {con++;frecuencias[i]=con;}
+    	}
+    	con=0;
+    	printf("la fruecunsia es :",con,n);
+    }
+}
+system("PAUSE");
+
+return 0;
+}
